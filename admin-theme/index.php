4e063f8ad7e349622ac0d707c259b3359e9b6f84
luizventurote <luiz_17@ymail.com>
2014-01-24 02:04:59 -0200

    Atualizacao.

--- admin-theme/index.php
+++ admin-theme/index.php
@@ -45,11 +45,101 @@
 
                 <div class="content" id="content">
 
-                    <?php include('/ui/title.php') ?>
+                    <div class="row">
 
-                    <?php include('/ui/logo.php') ?>
+                        <div class="col-sm-6">
 
-                    Teste Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, eos, perferendis totam soluta natus impedit ab quod in porro fugit? Ab, praesentium, odit voluptatum culpa facere dicta consectetur aut voluptate?
+                            <h1 class="page-title">Interfaces</h1>
+
+                        </div>
+
+                        <div class="col-sm-6">
+                        
+                            <ol class="breadcrumb">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Library</a></li>
+                                <li class="active">Data</li>
+                            </ol>
+
+                        </div>
+
+                    </div>
+
+                    <?php for ($i=0; $i < 3; $i++) { ?>
+
+                    <div class="row">
+
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Panel title</h3>
+                                </div>
+                                <div class="panel-body">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A reiciendis officia temporibus sit veniam veritatis corporis quibusdam illum nam nesciunt. Tempora, atque nesciunt iste sint laborum ex quis culpa ea.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Panel title</h3>
+                                </div>
+                                <div class="panel-body">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, odio, assumenda amet id magnam perspiciatis ratione ducimus voluptate quos quam nostrum qui cum dignissimos ullam natus laudantium animi commodi eum.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Panel title</h3>
+                                </div>
+                                <div class="panel-body">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, at, voluptatem, nam, consectetur eos earum ea quas sequi eum magni obcaecati veritatis id debitis consequatur nobis ipsum libero distinctio quo.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-lg-3">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Panel title</h3>
+                                </div>
+                                <div class="panel-body">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, vitae, neque totam placeat quisquam recusandae dolorem ducimus rem nesciunt iusto debitis quos accusamus cum dolorum fuga beatae nobis exercitationem molestias.
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <?php } ?>
+
+                    <div class="divider divider-single"></div>
+
+                    <footer class="row footer-main">
+                        
+                        <div class="col-sm-6">
+                            Desenvolvido por DevIFES
+                        </div>
+                        <div class="col-sm-6 logo-wrapper">
+
+                            <div class="logo">
+
+                                <img src="img/logo.png" alt="" class="logo-img">
+                                    
+                                <div class="info">
+                                    <div class="title-6 logo-title">Auxílio IFES | Campus Colatina</div>
+                                    <small class="copyright">Copyright 2013 - All Rights Reserved</small>
+                                </div>
+
+                            </div>
+                                
+                        </div>
+
+                    </footer>
 
                 </div>
                 
--- admin-theme/ui/menu.php
+++ admin-theme/ui/menu.php
@@ -2,27 +2,27 @@
 
 	<div class="menu-item-dropdown">
 
-		<div class="menu-item">Turmas <i class="fa fa-home fa-fw fa-lg icon"></i></div>
+		<div class="menu-item">Painel <i class="fa fa-home fa-fw fa-lg icon"></i></div>
 
 		<ul class="menu-dropdown">
 
-			<li><a class="item" href="#">Teste</a></li>
-			<li><a class="item" href="#">Teste</a></li>
-			<li><a class="item" href="#">Teste</a></li>
-			<li><a class="item" href="#">Teste</a></li>
-			<li><a class="item" href="#">Teste</a></li>
+			<li><a class="item" href="#">Painel</a></li>
+			<li><a class="item" href="#">Alunos</a></li>
+			<li><a class="item" href="#">Auxílios</a></li>
+			<li><a class="item" href="#">Processos</a></li>
+			<li><a class="item" href="#">Resultados</a></li>
 			
 		</ul>
 
 	</div>
 	
-	<div class="menu-item">Home <i class="fa fa-home fa-fw fa-lg icon"></i></div>
-	<div class="menu-item">Home <i class="fa fa-home fa-fw fa-lg icon"></i></div>
-	<div class="menu-item">Home <i class="fa fa-home fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">Alunos <i class="fa fa-user fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">Auxílios <i class="fa fa-cutlery fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">Processos <i class="fa fa-file-text fa-fw fa-lg icon"></i></div>
 
 	<div class="menu-item-dropdown">
 
-		<div class="menu-item">Turmas <i class="fa fa-home fa-fw fa-lg icon"></i></div>
+		<div class="menu-item">Resultados <i class="fa fa-check-square-o fa-fw fa-lg icon"></i></div>
 
 		<ul class="menu-dropdown">
 
@@ -35,5 +35,14 @@
 		</ul>
 
 	</div>
+
+	<div class="menu-item">Critérios <i class="fa fa-list-ol fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">Turmas <i class="fa fa-users fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">Cursos <i class="fa fa-book fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">Usuários <i class="fa fa-user fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">Recursos <i class="fa fa-clipboard fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">Pagamentos <i class="fa fa-usd fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">F.A.Q. <i class="fa fa-question fa-fw fa-lg icon"></i></div>
+	<div class="menu-item">Configurações <i class="fa fa-cog fa-fw fa-lg icon"></i></div>
 
 </nav>
