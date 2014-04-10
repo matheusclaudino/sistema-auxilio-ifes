db5311b04405502508590a1c0771a3578b694ef9
Paulo Henrique <2012122760136@col-7156-li05.cefetes.br>
2014-04-10 16:44:22 -0300

    turma

---
+++ model/Turma.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Turma
+ *
+ * @author 2012122760136
+ */
+class Turma {
+
+    //put your code here
+    private $turma_id;
+    private $curso_id;
+    private $nome;
+    private $ano;
+
+    function __construct($turma_id, $curso_id, $nome, $ano) {
+        $this->turma_id = $turma_id;
+        $this->curso_id = $curso_id;
+        $this->nome = $nome;
+        $this->ano = $ano;
+    }
+
+    public function getTurma_id() {
+        return $this->turma_id;
+    }
+
+    public function setTurma_id($turma_id) {
+        $this->turma_id = $turma_id;
+    }
+
+    public function getCurso_id() {
+        return $this->curso_id;
+    }
+
+    public function setCurso_id($curso_id) {
+        $this->curso_id = $curso_id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getAno() {
+        return $this->ano;
+    }
+
+    public function setAno($ano) {
+        $this->ano = $ano;
+    }
+
+}
+
+?>
