9745b6b79acd5ed53febeaf38061ad89df110a74
Paulo Henrique <2012122760136@col-7156-li05.cefetes.br>
2014-04-10 16:43:38 -0300

    parametro

---
+++ model/Parametro.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Parametro
+ *
+ * @author 2012122760136
+ */
+class Parametro {
+    //put your code here
+    private $parametro_id;
+    private $nome;
+    private $valor;
+    
+    function __construct($parametro_id, $nome, $valor) {
+        $this->parametro_id = $parametro_id;
+        $this->nome = $nome;
+        $this->valor = $valor;
+    }
+    
+    public function getParametro_id() {
+        return $this->parametro_id;
+    }
+
+    public function setParametro_id($parametro_id) {
+        $this->parametro_id = $parametro_id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getValor() {
+        return $this->valor;
+    }
+
+    public function setValor($valor) {
+        $this->valor = $valor;
+    }
+
+
+    
+
+
+
+}
+
+?>
