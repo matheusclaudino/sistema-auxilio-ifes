2e4cca3e2e9ab2c93ac73cbd9e42581d8f60341b
luizventurote <luiz_17@ymail.com>
2014-05-12 14:27:05 -0300

    AlunoView adicionado.

---
+++ admin/view/AlunoView.php
@@ -0,0 +1,29 @@
+<?php
+
+        require_once( '../autoload.php' );
+        
+        use Model\Fields;
+        
+        
+        
+        $fields = array();
+        
+        $fields[] = array(  'name' => 'Nome do aluno',
+                            'type' => 'text'
+                    );
+        $fields[] = array(  'name' => 'Nome do aluno',
+                            'type' => 'text'
+                    );
+        $fields[] = array(  'name' => 'Nome do aluno',
+                            'type' => 'text'
+                    );
+        $fields[] = array(  'name' => 'Senha',
+                            'type' => 'password'
+                    );
+        $fields[] = array(  'name' => 'Cadastrar',
+                            'type' => 'submit'
+                    );
+        
+        Fields::startFields( $fields );
+
+?>
