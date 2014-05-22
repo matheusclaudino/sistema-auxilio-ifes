d52fc70a72cc105c73b78dee688db281dabebc9b
luizventurote <luiz_17@ymail.com>
2014-05-22 14:51:06 -0300

    Logo e menu alterados.

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,6 +1,11 @@
 <?php
 
 /**
+ * Página inicial da área de administração
+ */
+define("ADMIN_URL", "http://localhost/sistema-auxilio-ifes/admin/");
+
+/**
  * Pasta do tema usado
  */
 define("THEME_PATH", "skin/admin/");
