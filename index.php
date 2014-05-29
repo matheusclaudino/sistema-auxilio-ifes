f89fd75090621ea5307a627433c0fcad7c83045b
Matheus Claudino <matheusclaudin@gmail.com>
2014-05-29 15:25:46 -0300

    Hierarquia de pasta alterada, e classe validaAcesso adicionada

--- index.php
+++ index.php
@@ -1,45 +1,5 @@
 <?php
 
-    $dados = array();
-    $dados = array('nome' => "Paulo Henrique",
-        'email' => "paulohenrique.de93@hotmail.com",
-        'senha' => "senhaaleatoria",
-        'rg' => "123456789",
-        'cpf' => "98765432100",
-        'naturaliade' => "brasileiro"
-    );
-
-    echo '<pre>';
-    print_r($dados);
-    echo '</pre><br><br>';
-
-    $campos = "";
-    $values = "";
-
-    foreach($dados as $campo => $valor)
-    {
-        
-        $campos = $campos.$campo.',';
-        
-        
-    }
-    
-    $campos = substr($campos, 0,-1);
-    $values = substr($values, 0,-1);
-    
-
-
-     echo '$stmt = $con->prepare(UPDATE $this->table SET('.$campos.')VALUES('.$values.')';
-     
-
-     foreach ($dados as $campo => $valor):
-
-         echo '$stmt->bindParam('.':'.$campo.',  '.$valor.')';
-             echo '<br/>';
-      
-     endforeach;
-
-      //$stmt->execute();
-
+   
   
 ?> 
---
+++ public/validaAcesso.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ echo $_GET["email"];
+ echo "<br/>";
+ echo $_GET["senha"];
+ 
+?>
