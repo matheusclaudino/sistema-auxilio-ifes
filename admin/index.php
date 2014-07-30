383ace2406ecf08c6d69aaec3beb5df2eac329dc
luizventurote <luiz_17@ymail.com>
2014-07-30 17:19:33 -0300

    Skin da Admin foi movido para outro diretório.

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+
+        <meta charset="UTF-8">
+        <title>Painel</title>
+        <meta name="description" content="Painel do sistema">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    </head>
+    <body>
+
+      
+
+    </body>
+</html>
